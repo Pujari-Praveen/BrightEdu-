f0cdb04d5f39cf261b82f52d7f17dc825d7a95d2
Pujari Praveen <srinip629@gmail.com>
2024-10-23 20:52:40 +0530

    Added few styles to the schedule input form and changed the structure of input boxes

--- src/ScheduleForm.jsx
+++ src/ScheduleForm.jsx
@@ -1,19 +1,17 @@
 const ExamsCountInput = () => {
   return (
-    <>
-      <label htmlFor="Exams-count-input" className="Exam-input-label">How many Exams Do you have</label>
+    <label className="Exam-input-label">How many Exams Do you have :
       <input type="number" id="Exams-count-input" className="Each-exam-input" required />
-    </>
+    </label>
   );
 };
 
 const InputForEachExam = () => {
   return (
-    <>
-      <label htmlFor="Exam-name-input" className="Exam-input-label">Name of the Exam and Date</label>
+    <label htmlFor="Exam-name-input" className="Exam-input-label">Name of the Exam and Date :
       <input type="text" id="Exam-name-input" className="Each-exam-input" required />
       <input type="date" className="Each-exam-input" required />
-    </>
+    </label>
   );
 };
 
