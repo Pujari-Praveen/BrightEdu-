34d8a9c940c64a95f2b15e020246a4197e8e251d
Pujari Praveen <srinip629@gmail.com>
2024-10-23 20:16:31 +0530

    Added navigation bar and logo in header

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+const NavigationBar = () => {
+  return (
+    <nav>
+      <a>serives</a>
+      <a>serives</a>
+      <a>serives</a>
+      <a>serives</a>
+    </nav>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="Main-header">
+      <img className="Logo" src='xyz' />
+      <h1 className='Logo-heading'>BrightEdu</h1>
+      <NavigationBar />
+    </header>
+  );
+};
+
+export default Header;
