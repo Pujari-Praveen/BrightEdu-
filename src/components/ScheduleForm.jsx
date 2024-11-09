f536101e70da3cec26abfb443a834293307c6e51
Pujari Praveen <srinip629@gmail.com>
2024-11-09 18:53:09 +0530

    Add carousel for multiple exam name inputs

--- src/components/ScheduleForm.jsx
+++ src/components/ScheduleForm.jsx
@@ -14,7 +14,7 @@ const AddInputBtn = () => {
 
 const EachExamInput = () => {
   return (
-    <div className="Form-group Each-exam-inputs-container">
+    <div className="Form-group Each-exam-inputs-container carousel-item active">
       <label htmlFor="Exam-name-input" className="Exam-input-label">Name of the Exam and Date :</label>
       <div>
         <input type="text" className="Each-exam-input Exam-name-input" placeholder='Enter the Name of the exam' required />
@@ -26,18 +26,20 @@ const EachExamInput = () => {
 
 const ExamInputsCarousel = () => {
   return (
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
+    <div id="carousel" className="carousel slide" data-bs-ride="false" data-bs-touch="false" data-bs-interval="false">
+      <div className="carousel-inner">
         <EachExamInput />
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+      <div className="Next-Exam-input-navigation">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 };
